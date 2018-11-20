db2f397d22fefc5d32d3953e81d974a1fec05b2b
Fraser Ross <fraser182@icloud.com>
2018-11-20 10:00:40 +0000

    homework checked

--- part_a/codeclan_students.rb
+++ part_a/codeclan_students.rb
@@ -1,34 +1,34 @@
 class Student
-
-  def initialize(name, cohort)
+#
+  def initialize(name, cohort) #this is what ruby looks for to create an instance of a class
     @name = name
     @cohort= cohort
   end
-
-  def get_student_name()
+#
+  def name()
     return @name
   end
 
-  def get_student_cohort()
+  def cohort()
     return @cohort
   end
 
 
-  def set_student_name(name)
-    @name = name
+  def set_name(new_name)
+    @name = new_name
   end
 
-  def set_student_cohort(cohort)
-    @cohort = cohort
+  def set_cohort(new_cohort) #method calls it
+    @cohort = new_cohort
   end
 
-  def get_student_to_talk(string)
-    return string
+  def talk() # every student of the student class gets access to this method.
+    return "I can talk"
   end
-
-  def favourite_language(language)
-    return "I love #{language}"
+#
+  def favourite_language(fav_language)
+    return "I love #{fav_language}"
 
   end
-
+#
 end
--- part_a/specs/codeclan_students_specs.rb
+++ part_a/specs/codeclan_students_specs.rb
@@ -2,37 +2,43 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../codeclan_students.rb')
 
-class TestStudent < MiniTest::Test
+class StudentTest < MiniTest::Test
 
 
-  def setup()
+  def setup() #runs before every single test in MiniTest
     @student = Student.new("Fraser", "G10")
   end
 
   def test_get_student_name()
-    assert_equal("Fraser", @student.get_student_name())
+    assert_equal("Fraser", @student.name) # links to def name
   end
 
   def test_get_student_cohort()
-    assert_equal("G10", @student.get_student_cohort())
+    assert_equal("G10", @student.cohort)
   end
 
   def test_set_student_name()
-    @student.set_student_name("Ally")
-    assert_equal("Ally", @student.get_student_name())
+    @student.set_name("Ally") # change students name
+    assert_equal("Ally", @student.name)
   end
 
   def test_set_student_cohort()
-    @student.set_student_cohort("G6")
-    assert_equal("G6", @student.get_student_cohort())
+    @student.set_cohort("G6")
+    assert_equal("G6", @student.cohort)
   end
 
-  def test_get_student_to_talk
-    assert_equal("I can talk", @student.get_student_to_talk("I can talk"))
+  def test_student_can_talk()
+    result = @student.talk()
+    assert_equal("I can talk", result)
   end
-
-  def test_say_favourite_language
-    assert_equal("I love Ruby", @student.favourite_language("Ruby"))
+# OR
+  # def test_student_can_talk()
+  #   assert_equal("I can talk", @student.talk())
+  # end
+  #
+  def test_student_has_favourite_language
+    result = @student.favourite_language("Ruby")
+    assert_equal("I love Ruby", result)
   end
 
 
